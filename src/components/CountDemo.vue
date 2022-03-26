f69de43e1c81ddac0d1a33ab07068d61eecd1661
yaeher/mawenzhen <mawenzhen@yaeher.com>
2022-03-26 19:28:52 +0800

    组合式 api 风格

--- src/components/CountDemo.vue
+++ src/components/CountDemo.vue
@@ -4,31 +4,47 @@
   </button>
 </template>
 
-<script>
-export default {
-  name: 'CountDemo',
-  // data 返回的属性将成为响应式的状态
-  // 并且暴露在 this 上
-  data() {
-    return {
-      count: 0
-    }
-  },
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'CountDemo',-->
+<!--  // data 返回的属性将成为响应式的状态-->
+<!--  // 并且暴露在 this 上-->
+<!--  data() {-->
+<!--    return {-->
+<!--      count: 0-->
+<!--    }-->
+<!--  },-->
 
-  // method 是一些用来更改状态与触发更新的函数
-  // 可以在模版中作为事件监听器绑定
-  methods: {
-    increment() {
-      this.count++
-    }
-  },
+<!--  // method 是一些用来更改状态与触发更新的函数-->
+<!--  // 可以在模版中作为事件监听器绑定-->
+<!--  methods: {-->
+<!--    increment() {-->
+<!--      this.count++-->
+<!--    }-->
+<!--  },-->
 
-  // 生命周期钩子会在组件生命周期的各个不同的阶段被调用
-  // 例如这个函数就会在组件挂载完成后被调用
-  mounted() {
-    console.log(`The initial count is ${this.count}.`)
+<!--  // 生命周期钩子会在组件生命周期的各个不同的阶段被调用-->
+<!--  // 例如这个函数就会在组件挂载完成后被调用-->
+<!--  mounted() {-->
+<!--    console.log(`The initial count is ${this.count}.`)-->
+<!--  }-->
+<!--}-->
+<!--</script>-->
+
+<script setup>
+  import {ref,onMounted} from 'vue'
+  // 响应式状态
+  const count = ref(0)
+
+  // 用来修改状态，触发更新的函数
+  function increment(){
+    count.value++
   }
-}
+
+  onMounted(()=>{
+    console.log(`The initial count is ${count.value}`)
+  })
+
 </script>
 
 <style scoped>
